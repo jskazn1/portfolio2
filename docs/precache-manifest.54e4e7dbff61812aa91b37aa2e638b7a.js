c833687ddcabb242ffecc260b5122908903d29bb
Jordan Kussmann <jordankussmann@Jordans-MacBook-Pro.local>
2020-02-12 10:16:59 -0800

    first commit

---
+++ docs/precache-manifest.54e4e7dbff61812aa91b37aa2e638b7a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bb91f2b7f6ae380af37931042c30522",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "6bf7d7a3bb988204a62f",
+    "url": "/portfolio/static/css/main.b28f0fb4.chunk.css"
+  },
+  {
+    "revision": "3f7c1abb021fc6586ab9",
+    "url": "/portfolio/static/js/2.9bc43008.chunk.js"
+  },
+  {
+    "revision": "6bf7d7a3bb988204a62f",
+    "url": "/portfolio/static/js/main.3d1f044e.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "4317be933b8b615a4b7847d40093ae3f",
+    "url": "/portfolio/static/media/header.4317be93.jpg"
+  }
+]);
